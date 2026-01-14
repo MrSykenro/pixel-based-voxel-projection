068c0df95e5b9ee33a20e6407b4d053466cfba7e
MrSykenro <kemalguksek@gmail.com>
2026-01-14 15:30:39 +0300

    Created preset for hip-cpu
    Created new preset called linux-intel-cpu
    This will allow compiling HIP/ROCm code on cpu with no changes to the code

---
+++ cmake/hip-cpu-wrapper.cmake
@@ -0,0 +1,8 @@
+# Create a fake hip::host library for hip-cpu
+if(NOT TARGET hip::host)
+    find_package(Threads REQUIRED)
+    find_package(TBB REQUIRED)
+    
+    add_library(hip::host INTERFACE IMPORTED)
+    target_link_libraries(hip::host INTERFACE hip_cpu_rt::hip_cpu_rt)
+endif()
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     int N = 100;
-    std::vector<float> a(N, 15.0f), b(N, 2.0f), c(N, 0.0f);
+    std::vector<float> a(N, 10.0f), b(N, 2.0f), c(N, 0.0f);
 
     // Call the wrapper
     run_vector_add(a, b, c, N);
