f21848bdbcfda318d12c79ab04e575d3f4b17b1a
MrSykenro <kemalguksek@gmail.com>
2026-01-27 22:33:09 +0300

    Fixed hip compilation error
    removed unneseccary file

--- lib/voxel_projection.hpp
+++ lib/voxel_projection.hpp
@@ -1,13 +1,18 @@
 #pragma once
-#include "formats.hpp"
-#include <hip/hip_runtime.h>
-#include <iostream>
+#define _USE_MATH_DEFINES
 
+#include "formats.hpp"
+#include <iostream>
+#include <math.h>
+
+#ifdef __HIPCC__
+#include <hip/hip_runtime.h>
 extern __constant__ Matrix3x3f c_camera_matrices[32];
 extern __constant__ float3 c_camera_positions[32];
 extern __constant__ float c_focal_lengths[32];
+#endif
 
-namespace VoxelP{extern "C"{    
+namespace VoxelP{ extern "C"{    
 /**
  * Sets the camera data into constant memory.
  * The array is limited by 32 elements (cameras).
@@ -15,5 +20,4 @@ namespace VoxelP{extern "C"{
  * @param camera_count The number of that cameras are included in the array. (Limited by 32)
  */
 void setCameraConstants(const CameraData* camera_data_array, int camera_count);
-}
-}
+}}
