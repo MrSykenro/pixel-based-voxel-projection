f12b27416032673ceb92611a22fc8f4f33c4b04e
MrSykenro <kemalguksek@gmail.com>
2026-02-03 23:19:25 +0300

    Added ray generation (without intensity)
    Combined camera constant and ray generation functions into a single file.
    Added kernel function to generate rays.
    Added helper function to create rays.

--- lib/formats.hpp
+++ lib/formats.hpp
@@ -13,3 +13,13 @@ struct CameraData
     float pos_x, pos_y, pos_z; 
 };
 
+// A ray buffer to pass array pointers that hold individual components of rays
+struct RayBuffer
+{
+    float* dir_x;
+    float* dir_y;
+    float* dir_z;
+    float* intensity;
+    int* camera_id;
+};
+
